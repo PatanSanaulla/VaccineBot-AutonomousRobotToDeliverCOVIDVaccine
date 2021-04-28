8ab2f4f64d2fdad8012c480a810de9459fd02fe5
PatanSanaulla <spatankhan@gmail.com>
2021-04-27 20:32:20 -0400

    creating a function for reading the camera input

--- cameraUtility.py
+++ cameraUtility.py
@@ -61,43 +61,44 @@ def detectOBI(image):
             pivotright(degrees)
             
     return image
-    
-#cv2.drawContours(image, contours
-# initialize the Raspberry Pi camera
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 25
-rawCapture = PiRGBArray(camera, size=(640,480))
 
-# allow the camera to warmup
-time.sleep(0.1)
 
-# define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('trackblockandretrive.avi', fourcc, 10, (640, 480))
-# write frame to video file
+def startCamera():
+    # initialize the Raspberry Pi camera
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 25
+    rawCapture = PiRGBArray(camera, size=(640,480))
 
-# keep looping
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=False):
-    # grab the current frame
-    start = datetime.datetime.now()
-    
-    image = frame.array
-    image = cv2.rotate(image, cv2.ROTATE_180)
-    
-    processedImage = detectOBI(image)
-    out.write(processedImage)
-    
-    # show the frame to our screen
-    cv2.imshow("Frame", processedImage)
-       
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-    # press the 'q' key to stop the video stream
-    if key == ord("q"):
-        gameover()
-        break
-    
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    # define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('trackblockandretrive.avi', fourcc, 10, (640, 480))
+    # write frame to video file
+
+    # keep looping
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=False):
+        # grab the current frame
+        start = datetime.datetime.now()
+        
+        image = frame.array
+        image = cv2.rotate(image, cv2.ROTATE_180)
+        
+        processedImage = detectOBI(image)
+        out.write(processedImage)
+        
+        # show the frame to our screen
+        cv2.imshow("Frame", processedImage)
+           
+        key = cv2.waitKey(1) & 0xFF
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        # press the 'q' key to stop the video stream
+        if key == ord("q"):
+            gameover()
+            break
+        
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
