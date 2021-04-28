618834983b9636f2b6fb1c0a794b7ec97c73a68c
PatanSanaulla <spatankhan@gmail.com>
2021-04-27 20:33:15 -0400

    adding threads to control the code

--- launchRobot.py
+++ launchRobot.py
@@ -5,3 +5,27 @@
 import emailUtility as EMAIL 
 import controlUtiltiy as CNTRL 
 import cameraUtility as CMRA
+from threading import Thread 
+
+
+
+def getCameraData:
+	#global CURRENTIMAGE
+	CURRENTIMAGE = CMRA.CURRENTIMAGE
+	print(CURRENTIMAGE)
+
+def startCompetetion():
+	try:
+		EMAIL.checkStartEmail() == True:
+
+			cameraThread = Thread(target = getCameraData)
+			cameraThread.start()
+
+	except:
+		print("Failed to start the Competetion.")
+
+
+
+
+if __name__ == "__main__":
+	startCompetetion()
