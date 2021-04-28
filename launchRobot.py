e2e782d0e15f7feb57558d8fb7156fda35849e06
PatanSanaulla <spatankhan@gmail.com>
2021-04-28 18:11:38 -0400

    adding the code to run the competition

--- launchRobot.py
+++ launchRobot.py
@@ -11,18 +11,35 @@ from threading import Thread
 import numpy as np
 import sys
 
+#Global Variables
+VACCINESTRANSPORTED = 0
+VACCINEGRIPPED = False
+VACCINEDELIVERED = False
+VACCINETYPE = ''
 
 
-def readCameraImage(Camera01):    
-    while True:
-        time.sleep(0.1)
-        currentImage = Camera01.getCurrentImage()
+def Competetion(Camera01, Controls01, _):
+	global VACCINESTRANSPORTED
+
+    while VACCINESTRANSPORTED != 3:
+
+    	if VACCINEGRIPPED == False:
+    		VACCINETYPE = Camera01.detectQRCode()
+    		VACCINEGRIPPED = True
+
+    	if VACCINEGRIPPED == True and VACCINEDELIVERED == False:
+    		#code to move ahead
+    		print("Reached Here!")
+
+
+    		#currentImage = Camera01.getCurrentImage()
+        #print()
 #         print('in here')
-        if isinstance(currentImage, np.ndarray):
+        #if isinstance(currentImage, np.ndarray):
             #Camera01.recognizeFace()
             #print(CNTRL.getIMUReading())
             #print(CNTRL.getDistance())
-            print("Got Image")
+            #print("Got Image")
             #cv2.imshow("Frame", CURRENTIMAGE)    
             
 
@@ -35,23 +52,21 @@ def startCompetetion():
             #cameraThread.daemon = True
             cameraThread.start()
             
-            IMUThread = Thread(target = CNTRL.IMUReading)
+            Controls01 = CNTRL.Controls()
+            IMUThread = Thread(target = Controls01.IMUReading, args = (Controls01, ))
             IMUThread.daemon = True
             IMUThread.start()
             
-            DistanceThread = Thread(target = CNTRL.distanceReading)
+            DistanceThread = Thread(target = Controls01.distanceReading, args = (Controls01, ))
             DistanceThread.daemon = True
             DistanceThread.start()
             
-            readImageThread = Thread(target = readCameraImage)
-            readImageThread.daemon = True
-            readImageThread.start()
-            
-            while True:
-                if key == ord("q"):
-                    sys.exit()
-                    break
+            Competetion = Thread(target = Competetion, args = (Camera01, Controls01, ))
+            Competetion.start()
 
+
+
+                
     except Exception as e: print(e)
             #print("Failed to start the Competetion.")
 
