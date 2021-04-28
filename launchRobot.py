b089c058b9aed0b068a2514c18619aceb56751a2
PatanSanaulla <spatankhan@gmail.com>
2021-04-28 00:30:59 -0400

    adding the code to start multiple threads to read the data

--- launchRobot.py
+++ launchRobot.py
@@ -1,31 +1,62 @@
-#Created by Patan Sanaulla KHan
+#Created by Patan Sanaulla Khan
 #Course 809T Building an Autonomous Robot
 
-
 import emailUtility as EMAIL 
-import controlUtiltiy as CNTRL 
+import controlUtility as CNTRL 
 import cameraUtility as CMRA
-from threading import Thread 
+import cv2
+import time
+from threading import Thread
+import numpy as np
+import sys
 
 
 
-def getCameraData:
-	#global CURRENTIMAGE
-	CURRENTIMAGE = CMRA.CURRENTIMAGE
-	print(CURRENTIMAGE)
+def readCameraImage():    
+    while True:
+        time.sleep(0.1)
+        CURRENTIMAGE = CMRA.getCurrentImage()
+#         print('in here')
+        if isinstance(CURRENTIMAGE, np.ndarray):
+            print(CNTRL.getIMUReading())
+            print(CNTRL.getDistance())
+            #print("Got Image")
+            #cv2.imshow("Frame", CURRENTIMAGE)
+            
+            
 
 def startCompetetion():
-	try:
-		EMAIL.checkStartEmail() == True:
+    try:
+        if EMAIL.checkStartEmail() == True:
+            
+            cameraThread = Thread(target = CMRA.startCamera)
+            #cameraThread.daemon = True
+            cameraThread.start()
+            
+            IMUThread = Thread(target = CNTRL.IMUReading)
+            IMUThread.daemon = True
+            IMUThread.start()
+            
+            DistanceThread = Thread(target = CNTRL.distanceReading)
+            DistanceThread.daemon = True
+            DistanceThread.start()
+            
+            #MapThread = 
+            
+            readImageThread = Thread(target = readCameraImage)
+            readImageThread.daemon = True
+            readImageThread.start()
+            
+            while True:
+                if key == ord("q"):
+                    sys.exit()
+                    break
 
-			cameraThread = Thread(target = getCameraData)
-			cameraThread.start()
-
-	except:
-		print("Failed to start the Competetion.")
+    except:
+        print("Failed to start the Competetion.")
 
 
 
 
 if __name__ == "__main__":
-	startCompetetion()
+    startCompetetion()
