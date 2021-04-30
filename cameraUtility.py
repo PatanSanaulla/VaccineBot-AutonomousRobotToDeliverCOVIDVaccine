4206c365d2162a3ec9240740ddd3862e01503cb9
PatanSanaulla <spatankhan@gmail.com>
2021-04-30 03:20:53 -0400

    tested and added the code to run face recognition

--- cameraUtility.py
+++ cameraUtility.py
@@ -13,6 +13,8 @@ BLUE_THRESHOLD = ((150, 90, 50), (180, 255, 255)) #TO BE FIXED
 
 THRESHOLDS = {"PFIZER":GREEN_THRESHOLD, "MODERNA":RED_THRESHOLD, "J&J": BLUE_THRESHOLD}
 
+NEURAL_NET = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
+
 class Camera:
 
     def __init__(self):
@@ -29,9 +31,6 @@ class Camera:
         X = 0
         Y = 0
         radius = 0.0
-        #global forwardCount
-#         height = (image.shape[0])
-#         width = (image.shape[1])
         
         #getting the HSV
         hsvImage = cv2.cvtColor(self.getCurrentImage(), cv2.COLOR_BGR2HSV)
@@ -51,7 +50,6 @@ class Camera:
         else:
             c = max(contours, key=cv2.contourArea)
             ((X,Y), radius) = cv2.minEnclosingCircle(c)
-            print('can see the object')
             return (True, X, Y, radius)
         
             #image = cv2.circle(image, (int(X),int(Y)), int(radius), (0, 0, 255), 2)
@@ -111,64 +109,59 @@ class Camera:
         #cv2.destroyAllWindows()
         
     def recognizeFace(self):
-        time.sleep(2.0)
+        
+        print('[INFO] Looking for Person...')
+        
+        faceNotFound = True
         # loop over the frames from the video stream
-        #while True:
-        # grab the frame from the threaded video stream and resize it
-        # to have a maximum width of 400 pixels
-        frame = self.image
-        frame = imutils.resize(frame, width=400)
+        while faceNotFound:
+            frame = self.getCurrentImage()
+            # grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
+            frame = imutils.resize(frame, width=400)
  
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-            (300, 300), (104.0, 177.0, 123.0))
+            # grab the frame dimensions and convert it to a blob
+            (h, w) = frame.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                (300, 300), (104.0, 177.0, 123.0))
  
-        # pass the blob through the network and obtain the detections and predictions
-        net.setInput(blob)
-        detections = net.forward()
+            # pass the blob through the network and obtain the detections and predictions
+            NEURAL_NET.setInput(blob)
+            detections = NEURAL_NET.forward()
 
-        # loop over the detections
-        for i in range(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with the prediction
-            confidence = detections[0, 0, i, 2]
+            # loop over the detections
+            for i in range(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with the prediction
+                confidence = detections[0, 0, i, 2]
 
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence < 0.5:
-                continue
+                # filter out weak detections by ensuring the `confidence` is
+                # greater than the minimum confidence
+                if confidence < 0.5:
+                    continue
 
-            # compute the (x, y)-coordinates of the bounding box for the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+                # compute the (x, y)-coordinates of the bounding box for the object
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
  
-            # draw the bounding box of the face along with the associated probability
-            text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-
-            # show the output frame
-            #self.out.write(frame)
+                # draw the bounding box of the face along with the associated probability
+                text = "{:.2f}%".format(confidence * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+                
+                cv2.imwrite("FaceRecognitionImage.jpg",frame)
+                faceNotFound = False
+                break
+        
+        return True
             
-            # show the frame to our screen
-            #cv2.imshow("Frame", frame)
-            #cv2.imshow("Frame", cv2.flip(frame,-1))
-            #key = cv2.waitKey(1) & 0xFF
- 
-            # if the `q` key was pressed, break from the loop
-            #if key == ord("q"):
-            #    break
-
-            #cv2.destroyAllWindows()
 
     def detectQRCode(self):
+        print('[INFO] Looking for QR Code...')
         #define detector
         detector = cv2.QRCodeDetector()
 
         while True:
             #check, img = cap.read() #check is a bool
-            print('[INFO] Looking for QR Code...')
             img = self.getCurrentImage()
             data, bbox, _ = detector.detectAndDecode(img)
             if(bbox is not None):
